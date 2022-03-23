2d5bb4b9049f79a942c7a3a4dfd833366e4cd3cf
Danila <danpry@st.amu.edu.pl>
2022-03-23 20:05:57 +0100

    Initial commit

---
+++ app/src/main/java/com/enctyptednotepad/MainActivity.java
@@ -0,0 +1,35 @@
+package com.enctyptednotepad;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText eName;
+    private EditText ePassword;
+    private Button eLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        eName = findViewById(R.id.etName);
+        ePassword = findViewById(R.id.etPassword);
+        eLogin = findViewById(R.id.etLogin);
+
+        eLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String inputName = eName.getText().toString();
+                String inputPassword = ePassword.getText().toString();
+
+                
+            }
+        });
+    }
+}
