b8bedbd22b70194602260903d8e620ff54847ed9
Danila <danpry@st.amu.edu.pl>
2022-03-23 21:23:45 +0100

    Basic Login Password authentification

--- app/src/main/java/com/enctyptednotepad/MainActivity.java
+++ app/src/main/java/com/enctyptednotepad/MainActivity.java
@@ -1,8 +1,11 @@
 package com.enctyptednotepad;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
     private EditText eName;
     private EditText ePassword;
     private Button eLogin;
+
+    private String Username = "Admin";
+    private String Password = "1234";
+
+    boolean isValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +36,32 @@ public class MainActivity extends AppCompatActivity {
                 String inputName = eName.getText().toString();
                 String inputPassword = ePassword.getText().toString();
 
-                
+                if (inputName.isEmpty() || inputPassword.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Short", Toast.LENGTH_LONG).show();
+                } else  {
+                    isValid = validate(inputName, inputPassword);
+
+                    if (!isValid) {
+                        Toast.makeText(MainActivity.this, "Wrong login or password", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_LONG).show();
+
+                        // new activity
+
+                        Intent intent = new Intent(MainActivity.this, HomePageActivity.class);
+                        startActivity(intent);
+                    }
+                }
             }
         });
+    }
+
+    private boolean validate (String name, String password) {
+
+        if (name.equals(Username) && password.equals(Password)) {
+            return true;
+        }
+
+        return false;
     }
 }
